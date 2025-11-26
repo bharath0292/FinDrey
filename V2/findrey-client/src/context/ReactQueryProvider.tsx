c8636b67d5fddcadba35b11a96da3c404a4d09af
bharath0292 <bharath0292@gmail.com>
2025-11-26 07:30:20 +0530

    Migrated to Biome

--- V2/findrey-client/src/context/ReactQueryProvider.tsx
+++ V2/findrey-client/src/context/ReactQueryProvider.tsx
@@ -1,29 +1,29 @@
 'use client';
 
-import { QueryClient, QueryClientProvider } from 'react-query';
-import { ReactQueryDevtools } from 'react-query/devtools';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      retry: 0,
-      refetchOnWindowFocus: false,
-      refetchOnMount: false,
-      refetchOnReconnect: false,
-      staleTime: 5 * 60 * 1000,
-    },
-  },
+	defaultOptions: {
+		queries: {
+			retry: 0,
+			refetchOnWindowFocus: false,
+			refetchOnMount: false,
+			refetchOnReconnect: false,
+			staleTime: 5 * 60 * 1000,
+		},
+	},
 });
 
 export function ReactQueryProvider({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <QueryClientProvider client={queryClient}>
-      {children}
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
-  );
+	return (
+		<QueryClientProvider client={queryClient}>
+			{children}
+			<ReactQueryDevtools initialIsOpen={false} />
+		</QueryClientProvider>
+	);
 }
